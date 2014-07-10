c7b6c38b64550227fa0d76f2c7ba9558d240bab5
Parker Smith <parker@parkersmith.us>
2014-07-10 15:26:03 -0600

    Add test namespace

--- tests/AbstractImplemention.php
+++ tests/AbstractImplemention.php
@@ -1,4 +1,4 @@
-<?php 
+<?php namespace Decouplr\Tests;
 
 class AbstractImplemention extends \Decouplr\Decouplr implements AbstractInterface {
 
--- tests/AbstractInterface.php
+++ tests/AbstractInterface.php
@@ -1,4 +1,4 @@
-<?php 
+<?php namespace Decouplr\Tests;
 
 interface AbstractInterface {
     public function doStuff($arg1, $arg2)
