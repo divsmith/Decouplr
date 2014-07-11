708121fd8159282bae048a3e30cef11c47935722
Parker Smith <parker@parkersmith.us>
2014-07-10 19:24:43 -0600

    Change $dependency to $decoupled

--- src/Decouplr.php
+++ src/Decouplr.php
@@ -2,18 +2,18 @@
 
 abstract class Decouplr {
 
-    protected $dependency;
+    protected $decoupled;
 
     public function __construct($dependency)
     {
-        $this->dependency = $dependency;
+        $this->decoupled = $dependency;
     }
 
     public function __call($method, $args)
     {
-        if( is_callable([$this->dependency, $method]) )
+        if( is_callable([$this->decoupled, $method]) )
         {
-            return call_user_func_array([$this->dependency, $method], $args);
+            return call_user_func_array([$this->decoupled, $method], $args);
         }
 
         throw new \BadMethodCallException();
